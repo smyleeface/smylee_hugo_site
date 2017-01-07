ec31aa2b9e4f5f0d35badb4eb1bf5e177acb39ce
smyleeface <patty.ramert@gmail.com>
2017-01-07 10:40:06 -0800

    make travis mad

--- tests/test_check_markdowns.py
+++ tests/test_check_markdowns.py
@@ -3,7 +3,7 @@ import unittest
 
 class TestCheckMarkdown(unittest.TestCase):
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual('foo'.upper(), 'FOmO')
 
 if __name__ == '__main__':
     unittest.main()
