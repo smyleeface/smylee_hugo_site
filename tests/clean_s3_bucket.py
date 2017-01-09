967e4dd3bbf5b57f1ef5b969831fa25761ca5e48
smyleeface <patty.ramert@gmail.com>
2017-01-08 20:21:33 -0800

    added region

--- tests/clean_s3_bucket.py
+++ tests/clean_s3_bucket.py
@@ -10,9 +10,10 @@ def kms_decrypt(secret_key):
 
 
 def clean_s3():
+    aws_region = 'us-west-2'
     access_key = 'AKIAJ7S3FKFV55L3GKSQ'
     secret_key = 'AQECAHhC2ka8BPeHckHRaHVXFFQT7C1GRB8vlWpPeDcwW1pOfAAAAIcwgYQGCSqGSIb3DQEHBqB3MHUCAQAwcAYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAzVqpvbTZPVTSVvO9ICARCAQ2m9ib/UFAB/iPXkq+LBUr4Z0hJHj0auNaK1o45f8PSVsAhRWuYzf6qLNBp8n0s5jikpKy/N8hTemaaILWaBNxNMH2Y='
-    s3 = boto3.resource('s3', aws_access_key_id=access_key, aws_secret_access_key=kms_decrypt(secret_key))
+    s3 = boto3.resource('s3', aws_access_key_id=access_key, aws_secret_access_key=kms_decrypt(secret_key), region=aws_region)
     bucket = s3.Bucket('smylee.com.updates')
     response = bucket.objects.delete()
     for deleted in response:
