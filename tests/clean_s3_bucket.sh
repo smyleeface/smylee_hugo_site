9f2b18878063bfef63ba46d87d017ced33fea513
smyleeface <patty.ramert@gmail.com>
2017-01-08 21:23:04 -0800

    reverting travis back, will clean bucket after hugo run.

--- tests/clean_s3_bucket.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "
-[default]
-aws_access_key_id = $ACCESS_KEY
-aws_secret_access_key = $SECRET_KEY" >~/.aws/credentials
-aws s3 rm s3://smylee.com.updates --recursive --region us-west-2
