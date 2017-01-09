4dc09688407c0dc8ceac6cdb9d27e5a17dd36668
smyleeface <patty.ramert@gmail.com>
2017-01-08 21:13:48 -0800

    adding credents

--- tests/clean_s3_bucket.sh
+++ tests/clean_s3_bucket.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+echo "
+[default]
+aws_access_key_id = ACCESS_KEY
+aws_secret_access_key = SECRET_KEY" >~/.aws/credentials
 aws s3 rm s3://smylee.com.updates --recursive --region us-west-2
