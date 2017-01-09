a8485202ad1dfdf2789e44ebba7e6cf198f127f5
smyleeface <patty.ramert@gmail.com>
2017-01-08 20:18:52 -0800

    added s3 credentials

--- tests/clean_s3_bucket.py
+++ tests/clean_s3_bucket.py
@@ -1,13 +1,23 @@
+import base64
 import boto3
 
 
+def kms_decrypt(secret_key):
+    kms = boto3.client('kms')
+    response = kms.decrypt(CiphertextBlob=base64.b64decode(secret_key))
+    secret = response['Plaintext']
+    return secret
+
+
 def clean_s3():
-    s3 = boto3.resource('s3')
+    access_key = 'AKIAJ7S3FKFV55L3GKSQ'
+    secret_key = 'AQECAHhC2ka8BPeHckHRaHVXFFQT7C1GRB8vlWpPeDcwW1pOfAAAAIcwgYQGCSqGSIb3DQEHBqB3MHUCAQAwcAYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAzVqpvbTZPVTSVvO9ICARCAQ2m9ib/UFAB/iPXkq+LBUr4Z0hJHj0auNaK1o45f8PSVsAhRWuYzf6qLNBp8n0s5jikpKy/N8hTemaaILWaBNxNMH2Y='
+    s3 = boto3.resource('s3', aws_access_key_id=access_key, aws_secret_access_key=kms_decrypt(secret_key))
     bucket = s3.Bucket('smylee.com.updates')
     response = bucket.objects.delete()
-    for deleted in response['Deleted']:
+    for deleted in response:
         print deleted['Key']
-    for errors in response['Errors']:
+    for errors in response:
         print errors['Key']
         print errors['Code']
         print errors['Message']
